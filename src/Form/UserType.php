2fb656ba031543e56c639e68bfe0a4fd4fd6fc48
Dolhen Mathieu <mathieu.dolhen@gmail.com>
2019-01-29 19:59:56 +0100

    Simplify registration needed, add UserType for ser modify

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,39 +5,45 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\CountryType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\LocaleType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
-class RegistrationFormType extends AbstractType
+class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+
         $builder
+            ->add('lastName', TextType::class)
+            ->add('firstName', TextType::class)
             ->add('email', EmailType::class)
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
+            ->add('genre', ChoiceType::class, [
+                'choices' => [
+                    'Femme' => true,
+                    'Homme' => false,
                 ],
+                'required' => true,
+            ])
+            ->add('birthDate', DateType::class, [
+                'required' => true,
+            ])
+            ->add('department', IntegerType::class, [
+                'required' => true
+            ])
+            ->add('city', TextType::class)
+            ->add('role', ChoiceType::class , [
+                'mapped' => false,
+                'choices' => [
+                    'Loueur' => true,
+                    'Participant' => false,
+                ]
             ])
         ;
     }
